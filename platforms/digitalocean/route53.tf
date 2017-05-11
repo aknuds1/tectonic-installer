c4b34196f11e77cb5ffb8e2ca820ea81d3440bdc
Aleks Saul <aleks.saul@gmail.com>
2017-05-10 23:06:50 -0400

    Updates to support etcd nodes on Digital Ocean
    - remove Route53 dependency
    - fixes https://github.com/coreos/tectonic-installer/issues/632 by changing the `--name=etcd` flag.
    - uses DO DNS for DNS record creation

--- platforms/digitalocean/route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_zone" "tectonic-int" {
-  name = "${var.tectonic_base_domain}"
-  force_destroy = true
-  tags = "${map(
-    "Name", "${var.tectonic_cluster_name}_tectonic_int_zone",
-    "KubernetesCluster", "${var.tectonic_cluster_name}"
-  )}"
-}
