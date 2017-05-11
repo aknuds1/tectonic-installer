584ce12c8442903aca323130ec2d5b2f32ddcd1d
Dan Gillespie <dan.gillespie@coreos.com>
2017-05-10 17:16:27 -0700

    *: filter logs for strings above certain length

--- installer/scripts/filter.sh
+++ installer/scripts/filter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # filter hides lines with words over MAX chars
 
-MAX=${MAX:-500}
-sed -e "s/[a-zA-Z0-9\/+]\{${MAX},\}/***OMITTED***/g"
+MAX=${MAX:-24}
+sed -e "s/[^\ ]\{${MAX},\}/***OMITTED***/g"
 
