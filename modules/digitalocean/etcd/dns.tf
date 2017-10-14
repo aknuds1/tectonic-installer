a97b966e58a1eea3424dfbbfc1f823ca753955a8
Nolan Reisbeck <nolan.reisbeck@gmail.com>
2017-10-13 17:48:19 -0700

    modules/digitalocean/etcd: remove subdomain of base_domain for etcd.

--- modules/digitalocean/etcd/dns.tf
+++ modules/digitalocean/etcd/dns.tf
@@ -1,14 +1,7 @@
-# Create separate domain for etcd nodes, because depending on main cluster domain would cause
-# a dependency cycle, as the latter must be instantiated with the API server IP address
-resource "digitalocean_domain" "etcd" {
-  name = "etcd.${var.base_domain}"
-  ip_address = "${digitalocean_droplet.etcd_node.*.ipv4_address[0]}"
-}
-
 resource "digitalocean_record" "etcd_nodes" {
-  count = "${var.droplet_count}"
-  domain = "${digitalocean_domain.etcd.id}"
-  type = "A"
-  name = "etcd-${count.index}"
-  value = "${digitalocean_droplet.etcd_node.*.ipv4_address[count.index]}"
+  count  = "${var.droplet_count}"
+  domain = "${var.base_domain}"
+  type   = "A"
+  name   = "${var.cluster_name}-etcd-${count.index}"
+  value  = "${digitalocean_droplet.etcd_node.*.ipv4_address[count.index]}"
 }
